03423df637761c2489797a66e31d0836e8873b8f
By Design <jonahpickens@protonmail.com>
2024-10-02 15:56:25 -0700

    Added Scripts folder with app.js

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+var app = (function () {
+    /* Properties */
+    var websiteName = "Space Tigers Shop";
+    /* Methods */
+    return {
+        getWebsiteName: function () {
+            return websiteName;
+        }
+    }
+}) ();
