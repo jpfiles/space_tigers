6923a81211892306d61197fd177ab0937474d7be
Elle1296 <joelle.nomura@outlook.com>
2024-10-10 13:44:11 -0700

    Updated models.py

--- models.py
+++ models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model): 
-    name = models.CharField(max_length=255) 
-    price = models.FloatField() 
-    stock = models.IntegerField() 
-    image_url = models.CharField(max_length=2083) 
+class Product(models.Model):
+   name = models.CharField(max_length=255)
+   price = models.FloatField()
+   stock = models.IntegerField()
+   image_url = models.CharField(max_length=2083)
+
+class Customer(models.Model):
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    mi = models.CharField(max_length=1, blank=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    avatar_url = models.URLField(blank=True)  
+
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
