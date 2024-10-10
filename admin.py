c11f248683aed53fd5f5e0641719072f5f22e9c1
Elle1296 <joelle.nomura@outlook.com>
2024-10-10 13:43:31 -0700

    updated admin.py

--- admin.py
+++ admin.py
@@ -1,8 +1,15 @@
-from django.contrib import admin
-from .models import Product
-# Register your models here.
+from django.contrib import admin 
+from .models import Customer, Product 
+
+# Register your models here. 
+ 
 
 class ProductAdmin(admin.ModelAdmin): 
-  list_display = ('name', 'price', 'stock') 
-  
-admin.site.register(Product)
+    list_display = ('name', 'price', 'stock') 
+
+    admin.site.register(Product)
+
+class CustomerAdmin(admin.ModelAdmin): 
+    list_display = ('lname', 'fname', 'mi', 'email', 'pnumber', 'ava_url') 
+
+    admin.site.register(Customer)
