51f72db3a55306913b06299700e7c94524ff97f6
Elle1296 <joelle.nomura@outlook.com>
2024-10-10 13:33:59 -0700

    Updated views.py

--- views.py
+++ views.py
@@ -1,18 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Product
-
+from .models import Product, Customer
 # Create your views here.
  
+def login(request):
+    return render(request, 'login.html')
+
 def index(request):
     Product_list = Product.objects.all()
     return render(request, 'index.html', {'products': Product_list})
- 
-def customer(request):
-    return render(request, 'customer.html')
 
-def table(request):
-    return render(request, 'table.html')
+def cust_list(request):
+    Customer_list = Customer.objects.all()
+    return render(request, 'cust_list.html',{'customers': Customer_list})
+ 
+def newcustomer(request):
+    return render(request, 'newcustomer.html')
 
 def contact_us(request):
     return render(request, 'contact_us.html')
