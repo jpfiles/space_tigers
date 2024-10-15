f8cd3580ea9b6f3db01f5b4b7c43f06604485262
By Design <jonahpickens@protonmail.com>
2024-10-15 13:53:53 -0700

    Updating urls to reflect update/delete functions

--- urls.py
+++ urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
- 
+
 urlpatterns = [
-   path('login/', views.login, name='login'),
-   path('index/', views.index, name="index"),
-   path('cust_list/', views.cust_list,name="cust_list"),
-   path('newcustomer/', views.newcustomer, name="newcustomer"),
-   path('contact_us/', views.contact_us, name="contact_us"),
+    path('login/', views.login, name="login"),
+    path('', views.index, name="index"),
+    path('cust_list/', views.cust_list, name="cust_list"),
+    path('newcustomer/', views.newcustomer, name="newcustomer"),
+    path('contact_us/', views.contact_us, name="contact_us"),
+    # New URLs for updating and deleting customers
+    path('customer/update/<int:id>/', views.update_customer, name='update_customer'),
+    path('customer/delete/<int:id>/', views.delete_customer, name='delete_customer'),
 ]
