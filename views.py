b89355ace44661a690a9feb7ae60874d9f4c1606
By Design <jonahpickens@protonmail.com>
2024-10-15 13:54:34 -0700

    Updating views.py

--- views.py
+++ views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, Customer
+from .forms import CustomerForm  # Assuming you have a ModelForm for Customer
+
 # Create your views here.
- 
+
 def login(request):
     return render(request, 'login.html')
 
@@ -12,13 +14,30 @@ def index(request):
 
 def cust_list(request):
     Customer_list = Customer.objects.all()
-    return render(request, 'cust_list.html',{'customers': Customer_list})
- 
+    return render(request, 'cust_list.html', {'customers': Customer_list})
+
 def newcustomer(request):
     return render(request, 'newcustomer.html')
 
 def contact_us(request):
     return render(request, 'contact_us.html')
 
-def new(request):
-    return HttpResponse('New Page')
+# Update Customer
+def update_customer(request, id):
+    customer = get_object_or_404(Customer, id=id)
+    if request.method == "POST":
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return redirect('cust_list')
+    else:
+        form = CustomerForm(instance=customer)
+    return render(request, 'update_customer.html', {'form': form})
+
+# Delete Customer
+def delete_customer(request, id):
+    customer = get_object_or_404(Customer, id=id)
+    if request.method == "POST":
+        customer.delete()
+        return redirect('cust_list')
+    return render(request, 'delete_confirm.html', {'object': customer})
